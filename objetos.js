48e4e007d6ef5f13ad6e98e1c38ecf59fce227a2
juansebastiangallegocarvajal <64376323+troi-k@users.noreply.github.com>
2021-08-28 17:25:06 -0500

    objetos y fizz

---
+++ objetos.js
@@ -0,0 +1,23 @@
+let estudiante={
+    nombre:"sebastian",
+    edad: 31,
+    carrera:"desarrollo de software",
+    genero:"M",
+    telefono: "3043406627",
+    estatura:1.85,
+    hinchaDelmejor:true,
+    materias:["calculo","fisica","logica"],
+    comidas:{
+        postre:"tiramizu",
+        fuerte:"frijoles",
+        bebida:"guaro",
+
+
+    }
+
+
+
+
+}
+
+console.log (estudiante.comidas.postre);
